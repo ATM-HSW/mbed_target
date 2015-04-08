7bf1bd4de6145d7c4449148c83db6571773afe28
hagendorf <olaf.hagendorf@hs-wismar.de>
2015-04-08 23:59:33 +0200

    Adding: - Encoder for F407 and F303 - SPI Master and Slave read/write

--- blocks/mex_compile_all.m
+++ blocks/mex_compile_all.m
@@ -1,49 +1,47 @@
-mex sfunar_analogInput.c
-mex sfunar_analogOutput.c
-mex sfunar_digitalInput.c
-mex sfunar_digitalOutput.c
-mex sfunar_lcdOutput.c
-mex sfunar_serialConfig.c
-mex sfunar_serialRead.c
-mex sfunar_serialWrite.c
 mex sfunar_PWMOutput.c
-
-
-mex sfunar_i2cConfig.c
 mex sfunar_adcMcp3428Config.c
-mex sfunar_analogInput_mcp3428.c
 mex sfunar_adcMcp4728Config.c
-mex sfunar_analogOutput_mcp4728.c
-
-mex sfunar_tmp102Config.c
-mex sfunar_temperature_tmp102.c
-
-mex sfunar_pcf8574Config.c
-mex sfunar_digitalInput_pcf8574.c
-mex sfunar_digitalOutput_pcf8574.c
-
-
-mex sfunar_spiConfig.c
-mex sfunar_max11300Config.c
+mex sfunar_analogInput.c
 mex sfunar_analogInput_max11300.c
-mex sfunar_analogOutput_max11300.c
-mex sfunar_temperature_max11300.c
-mex sfunar_digitalInput_max11300.c
-mex sfunar_digitalOutput_max11300.c
-
-mex sfunar_mcp3204Config.c
 mex sfunar_analogInput_mcp3204.c
-
-mex sfunar_mcp4822Config.c
+mex sfunar_analogInput_mcp3428.c
+mex sfunar_analogOutput.c
+mex sfunar_analogOutput_max11300.c
+mex sfunar_analogOutput_mcp4728.c
 mex sfunar_analogOutput_mcp4822.c
-
-mex sfunar_tmp123Config.c
-mex sfunar_temperature_tmp123.c
-
+mex sfunar_digitalInput.c
+mex sfunar_digitalInput_max11300.c
+mex sfunar_digitalInput_pcf8574.c
+mex sfunar_digitalOutput.c
+mex sfunar_digitalOutput_max11300.c
+mex sfunar_digitalOutput_pcf8574.c
+mex sfunar_encoderInput.c
+mex sfunar_encoderInput_F303RE.c
+mex sfunar_i2cConfig.c
 mex sfunar_i2cFramConfig.c
 mex sfunar_i2cFram_read.c
 mex sfunar_i2cFram_write.c
-
+mex sfunar_lcdOutput.c
+mex sfunar_max11300Config.c
+mex sfunar_mcp3204Config.c
+mex sfunar_mcp4822Config.c
+mex sfunar_pcf8574Config.c
+mex sfunar_serialConfig.c
+mex sfunar_serialRead.c
+mex sfunar_serialWrite.c
+mex sfunar_servoOutput.c
+mex sfunar_servoOutput.org.c
+mex sfunar_spiConfig.c
 mex sfunar_spiFramConfig.c
 mex sfunar_spiFram_read.c
 mex sfunar_spiFram_write.c
+mex sfunar_spiSlaveConfig.c
+mex sfunar_spiSlaveRead.c
+mex sfunar_spiSlaveWrite.c
+mex sfunar_spi_csConfig.c
+mex sfunar_spi_write.c
+mex sfunar_temperature_max11300.c
+mex sfunar_temperature_tmp102.c
+mex sfunar_temperature_tmp123.c
+mex sfunar_tmp102Config.c
+mex sfunar_tmp123Config.c
