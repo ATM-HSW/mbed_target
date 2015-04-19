71ad2f7d047875fa05483736935662c23102e0bd
ohagendorf <olaf.hagendorf@hs-wismar.de>
2015-04-19 23:33:35 +0200

    - reorg library structure - similar to mbed structur

--- blocks/slblocks.m
+++ blocks/slblocks.m
@@ -3,12 +3,15 @@
 %             2014 Dr. Olaf Hagendorf, HS Wismar
 
 function blkStruct = slblocks
-    blkStruct.Name        = 'Mbed Target'; %Display name
-    blkStruct.OpenFcn     = 'mbed_lib'; %Library name
-    blkStruct.MaskDisplay = '';
+  blkStruct.Name        = 'Mbed Target'; %Display name
+  blkStruct.OpenFcn     = 'mbed_lib'; %Library name
+  blkStruct.MaskDisplay = '';
 
-    Browser(1).Library    = 'mbed_lib'; %Library name
-    Browser(1).Name       = 'Mbed Target';%Display name
-    Browser(1).IsFlat  = 0;
+  Browser(1).Library    = 'mbed_lib'; %Library name
+  Browser(1).Name       = 'Mbed Target';%Display name
+  Browser(1).IsFlat  = 0;
 
-    blkStruct.Browser     = Browser;
+  blkStruct.Browser     = Browser;
+
+  % Define information for model updater
+  blkStruct.ModelUpdaterMethods.fhSeparatedChecks = @ecblksUpdateModel;
