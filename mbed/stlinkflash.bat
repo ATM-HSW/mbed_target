bd83473567c269efef7987c82609fd0696d809d2
Olaf Hagendorf <olaf.hagendorf@hs-wismar.de>
2015-04-23 13:05:49 +0200

    STLink flash: add verify after programming and disable progress bar

--- mbed/stlinkflash.bat
+++ mbed/stlinkflash.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI" -c SWD -P %1 0x08000000 -Rst -Run
+"C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI" -c SWD -P %1 0x08000000 -Rst -Run -Q -V after_programming
