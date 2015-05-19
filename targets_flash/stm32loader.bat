49ccbefcc1f8e2c71b0409e818115e51fe18f83b
ohagendorf <olaf.hagendorf@hs-wismar.de>
2015-05-20 00:06:46 +0200

    Adding full erase before flashing

--- targets_flash/stm32loader.bat
+++ targets_flash/stm32loader.bat
@@ -4,4 +4,4 @@ REM %3 mbed virtual drive
 REM %4 COM port
 
 python.exe %2\targets_flash\stm32loader_reset2bootloader.py
-python.exe %2\targets_flash\stm32loader.py -p %4 -w -g 0x08000000 %1 0x08000000 -Rst -Run -Q -V after_programming
+python.exe %2\targets_flash\stm32loader.py -p %4 -e -w -g 0x08000000 %1 0x08000000 -Rst -Run -Q -V after_programming
