9300ba6d6bc2aac85ee63e9ce3779bfe21da7c16
ohagendorf <olaf.hagendorf@hs-wismar.de>
2016-05-23 00:42:24 +0200

    i2c lib reorg, adding lm75b block

--- blocks/libraries/LM75B.cpp
+++ blocks/libraries/LM75B.cpp
@@ -1,15 +1,13 @@
 #include "LM75B.h"
- 
-LM75B::LM75B(PinName sda, PinName scl) : i2c(sda, scl)
-{
+
+LM75B::LM75B(I2C *i2c) {
    char cmd[2];
+	 _i2c = i2c;
    cmd[0]    = LM75B_Conf;
    cmd[1]    = 0x0;   
-   i2c.write( LM75B_ADDR, cmd, 2);
+   _i2c->write( LM75B_ADDR, cmd, 2);
 }
- 
- 
- 
+
 LM75B::~LM75B()
 {
  
@@ -20,7 +18,7 @@ float LM75B::read()
     char cmd[2];
     cmd[0] = LM75B_Temp;
    
-    i2c.write( LM75B_ADDR, cmd, 1); // Send command string
-    i2c.read( LM75B_ADDR, cmd, 2); // Send command string    
+    _i2c->write( LM75B_ADDR, cmd, 1); // Send command string
+    _i2c->read( LM75B_ADDR, cmd, 2); // Send command string    
     return (   float((cmd[0]<<8)|cmd[1]) / 256.0   );
 }
