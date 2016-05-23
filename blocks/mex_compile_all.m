fddca39a75098c758036994f6ba22de2898e081f
Olaf Hagendorf <olaf.hagendorf@hs-wismar.de>
2016-05-23 16:32:13 +0200

    rewrite compile_all
    checking file dates of .c and mexw64 files - compile only when .c is newer

--- blocks/mex_compile_all.m
+++ blocks/mex_compile_all.m
@@ -1,120 +1,18 @@
-%% general
-mex sfunar_PWMOutput.c
-mex sfunar_analogInput.c
-mex sfunar_analogOutput.c
-mex sfunar_createThread.c
-mex sfunar_counter.c
-mex sfunar_digitalInput.c
-mex sfunar_digitalOutput.c
-mex sfunar_i2cConfig.c
-mex sfunar_encoderInput.c
-mex sfunar_i2cRead.c
-mex sfunar_i2cWrite.c
-mex sfunar_inputCapture.c
-mex sfunar_Interrupt.c
-mex sfunar_lcdOutput.c
-mex sfunar_mailReceive.c
-mex sfunar_mailSend.c
-mex sfunar_randomNumber.c
-mex sfunar_runtimeGpio.c
-mex sfunar_runtimeDac.c
-mex sfunar_serialConfig.c
-mex sfunar_serialDmaConfig.c
-mex sfunar_serialDmaRead.c
-mex sfunar_serialDmaWrite.c
-mex sfunar_serialRead.c
-mex sfunar_serialWrite.c
-mex sfunar_spiConfig.c
-mex sfunar_spiDmaConfig.c
-mex sfunar_spiDmaRead.c
-mex sfunar_spiDmaWrite.c
-mex sfunar_spiSlaveConfig.c
-mex sfunar_spiSlaveRead.c
-mex sfunar_spiSlaveWrite.c
-mex sfunar_spi_write.c
-mex sfunar_usbSerialConfig.c
-mex sfunar_usbSerialRead.c
-mex sfunar_usbSerialWrite.c
-mex sfunar_DataloggerPlain.c
-mex sfunar_serialWriteBlock.c
-%%
+fi=dir('*.c');
 
-%% MCP3428
-mex sfunar_analogIn_mcp3428.c
-%%
-
-%% MCP4728
-mex sfunar_analogOut_mcp4728.c
-%%
-
-%% MAX11300
-mex sfunar_analogInput_max11300.c
-mex sfunar_analogOutput_max11300.c
-mex sfunar_digitalInput_max11300.c
-mex sfunar_digitalOutput_max11300.c
-mex sfunar_max11300Config.c
-mex sfunar_temperature_max11300.c
-
-%% MCP3204
-mex sfunar_analogIn_mcp3204.c
-%%
-
-%% MCP4822
-mex sfunar_analogOut_mcp4822.c
-%%
-
-%% PCF8574
-mex sfunar_digitalIn_pcf8574.c
-mex sfunar_digitalOut_pcf8574.c
-%%
-
-%% FRAM I2C
-mex sfunar_i2cFram_read.c
-mex sfunar_i2cFram_write.c
-%%
-
-%% FRAM SPI
-mex sfunar_spiFram_read.c
-mex sfunar_spiFram_write.c
-%%
-
-%% Temperature Sensors
-mex sfunar_temperature_tmp102.c
-mex sfunar_temperature_tmp123.c
-%%
-
-%% Display MI0283
-mex sfunar_mi0283qtWrite.c
-%%
-
-%% Arch Max
-mex sfunar_DataloggerSDCard.c
-%%
-
-%% DS3231 RTC
-mex sfunar_ds3231.c
-%%
-
-%% MPU9250 Gyro and Accel
-mex sfunar_mpu9250.c
-%%
-
-%% AK8963 Magnetometer
-mex sfunar_ak8963.c
-%%
-
-%% BMP180
-mex sfunar_bmp180.c
-%%
-
-%% LIS302DL Accelerometer
-mex sfunar_lis302dl_acc.c
-%%
-
-%% L3GD20 Gyroscope
-mex sfunar_l3gd20_gyro.c
-%%
-
-%% LSM303DLHC Accelerometer and Magnetometer
-mex sfunar_lsm303dlhc_acc_mag.c
-%%
+for i=1:size(fi,1)
+    [pathstr,name,ext] = fileparts(fi(i).name);
+    try
+    fi1=dir([name '.c']);
+    fi2=dir([name '.mexw64']);
+    if isempty(fi2)
+        disp(['compile ' name '.c']);
+        mex( [name '.c'])
+    elseif fi1.datenum>fi2.datenum
+        disp(['compile ' name '.c']);
+        mex( [name '.c'])
+    end
+    catch E
+        disp E;
+    end
+end
