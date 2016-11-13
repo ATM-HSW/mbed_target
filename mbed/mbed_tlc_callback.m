d5ccc56c59bf44fcec220702620af40d88b5f9cc
ohagendorf <olaf.hagendorf@hs-wismar.de>
2016-11-13 23:23:12 +0100

    mbed os 5 integration

--- mbed/mbed_tlc_callback.m
+++ mbed/mbed_tlc_callback.m
@@ -1,8 +1,27 @@
 function mbed_tlc_callback(hDlg, hSrc, paramName)
 %Callback to get/set mbed target properties
 
+if isequal(paramName, 'MbedVersion')
+    mbedversion = (slConfigUIGetVal(hDlg, hSrc, paramName));
+    if isequal(mbedversion, 'mbed-os 5')
+      slConfigUISetEnabled(hDlg, hSrc,'MbedTarget',0);
+      slConfigUISetEnabled(hDlg, hSrc,'MbedTarget5',1);
+    else
+      slConfigUISetEnabled(hDlg, hSrc,'MbedTarget',1);
+      slConfigUISetEnabled(hDlg, hSrc,'MbedTarget5',0);
+    end
+end
+
 if isequal(paramName, 'MbedDrive')
     mbed.Prefs.setMbedDrive(slConfigUIGetVal(hDlg, hSrc, paramName));
+end
+
+if isequal(paramName, 'mbedls')
+    [a,b]=system('mbedls -j');
+    c=loadjson(b);
+    slConfigUISetVal(hDlg, hSrc, 'MbedDrive', c{1}.mount_point);
+    slConfigUISetVal(hDlg, hSrc, 'ComPort', [c{1}.serial_port ':']);
+    slConfigUISetVal(hDlg, hSrc, 'MbedTarget5', c{1}.platform_name);
 end
 
 if isequal(paramName, 'DownloadApplicationmbed_tlc_callback')
