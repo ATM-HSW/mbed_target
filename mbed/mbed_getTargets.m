5e897fe544cd3b9b765c1bfa3fa27c1928c9c594
Olaf Hagendorf <olaf.hagendorf@hs-wismar.de>
2016-11-14 10:06:08 +0100

    Revert "mbed os 5 integration"
    This reverts commit d5ccc56c59bf44fcec220702620af40d88b5f9cc.

--- mbed/mbed_getTargets.m
+++ mbed/mbed_getTargets.m
@@ -1,20 +1,11 @@
-function ret = mbed_getTargets(mbedversion)
-
-  if isequal(mbedversion, 'mbed-os 5')
-    [a,b]=system('mbed export -T');
-    b=regexprep(b,'\n\n','');
-    ret=regexprep(b,'\n','|');
-    ret=ret(1:end-1);
-  else
-
-    pathstr = mbed_getTargetRootPath();
-    targetsfiles = ls(fullfile(pathstr,'targets','*.zip'));
-    ret=[];
-    for i=1:size(targetsfiles,1)
-        [~,name,~] = fileparts(targetsfiles(i,:));
-        ret = [ret '|' name]; %#ok<AGROW>
-    end
-    ret = ret(2:end);
-  end
-  disp(ret);
+function ret = mbed_getTargets()
+pathstr = mbed_getTargetRootPath();
+targetsfiles = ls(fullfile(pathstr,'targets','*.zip'));
+ret=[];
+for i=1:size(targetsfiles,1)
+    %ret(i)
+    [~,name,~] = fileparts(targetsfiles(i,:));
+    ret = [ret '|' name]; %#ok<AGROW>
+end
+ret = ret(2:end);
 end
