cdd384e63103a00cb59d564cd7f0b91998d4a391
ohagendorf <olaf.hagendorf@hs-wismar.de>
2017-04-26 19:03:09 +0200

    rebase error bugfix

--- mbed/mbed_getTargets.m
+++ mbed/mbed_getTargets.m
@@ -1,14 +1,3 @@
-function ret = mbed_getTargets_old()
-pathstr = mbed_getTargetRootPath();
-targetsfiles = ls(fullfile(pathstr,'targets','*.zip'));
-ret=[];
-for i=1:size(targetsfiles,1)
-    %ret(i)
-    [~,name,~] = fileparts(targetsfiles(i,:));
-    ret = [ret '|' name]; %#ok<AGROW>
-end
-ret = ret(2:end);
-
 function ret = mbed_getTargets(mbedversion)
 
   if isequal(mbedversion, 'mbed-os 5')
@@ -28,5 +17,4 @@ function ret = mbed_getTargets(mbedversion)
     ret = ret(2:end);
   end
   disp(ret);
->>>>>>> integration of pip installation of mbed-cli and mbed-ls
 end
