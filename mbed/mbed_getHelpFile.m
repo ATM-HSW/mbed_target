5684a4dbc3a5b6ec871916780d58f591488258b6
ohagendorf <olaf.hagendorf@hs-wismar.de>
2018-10-25 22:16:12 +0200

    doc/rework LM75B block

--- mbed/mbed_getHelpFile.m
+++ mbed/mbed_getHelpFile.m
@@ -50,6 +50,45 @@ function createStruct()
   addStruct('RTOSmailSend',             'rtos/Mail_Send_en');
   addStruct('RTOSmailReceive',          'rtos/Mail_Receive_en');
   addStruct('RTOSThreadwait',           'rtos/Threadwait_en');
+
+  addStruct('Max11300_Config',          'converter_extender/Max11300_Config_en');
+  addStruct('Max11300_digitalInput',    'converter_extender/Max11300_Digital_Input_en');
+  addStruct('Max11300_digitalOutput',   'converter_extender/Max11300_Digital_Output_en');
+  addStruct('Max11300_analogInput',     'converter_extender/Max11300_Analog_Input_en');
+  addStruct('Max11300_analogOutput',    'converter_extender/Max11300_Analog_Output_en');
+  addStruct('Max11300_temperature',     'converter_extender/Max11300_Temperature_en');
+  addStruct('mcp3428',                  'converter_extender/Mcp3428_Analog_Input_en');
+  addStruct('mcp4728',                  'converter_extender/Mcp4728_Analog_Output_en');
+  addStruct('PCF8574DigitalOutput',     'converter_extender/Pcf8574_Digital_Output_en');
+  addStruct('PCF8574DigitalInput',      'converter_extender/Pcf8574_Digital_Input_en');
+  addStruct('mcp4822',                  'converter_extender/Mcp4822_Analog_Output_en');
+  addStruct('mcp3204',                  'converter_extender/Mcp3204_Analog_Input_en');
+
+  addStruct('tmp102',                   'devices/TMP102_en');
+  addStruct('lm75b',                    'devices/LM75B_en');
+  addStruct('bmp180',                   'devices/BMP180_en');
+  addStruct('vl6180x',                  'devices/VL6180X_en');
+  addStruct('lsm303dlhc',               'devices/LSM303DLHC_en');
+  addStruct('mpu9250',                  'devices/MPU9250_en');
+  addStruct('mpu9250_ak8963',           'devices/MPU9250_AK8963_en');
+  addStruct('tmp123',                   'devices/TMP123_en');
+  addStruct('lis302dl',                 'devices/LIS302DL_en');
+  addStruct('l3gd20',                   'devices/L3GD20_en');
+  addStruct('max6675',                  'devices/MAX6675_en');
+  addStruct('ds1820',                   'devices/DS1820_en');
+  addStruct('DisplayCR12832',           'devices/DisplayCR12832_en');
+  addStruct('DS3231',                   'devices/DS3231_en');
+
+  addStruct('fm24cl16bRead',            'memory/Fm24cl16b_Fram_Read_en');
+  addStruct('fm24cl16bWrite',           'memory/Fm24cl16b_Fram_Write_en');
+  addStruct('fm25l16bRead',             'memory/Fm25l16b_Fram_Read_en');
+  addStruct('fm25l16bWrite',            'memory/Fm25l16b_Fram_Write_en');
+
+  addStruct('OneWireConfig',            'serial/one_wire_config_en');
+  addStruct('OneWireReadROM',           'serial/one_wire_search_rom_en');
+  addStruct('TLC5952Config',            'serial/TLC5952Config_en');
+  addStruct('TLC5962Ventil',            'serial/TLC5962Ventil_en');
+  addStruct('TLC5952Balken',            'serial/TLC5952Balken_en');
   addStruct('SerialUARTConfig',         'serial/UART_Config_en');
   addStruct('SerialUARTConfigUSB',      'serial/UART_ConfigUSB_en');
   addStruct('SerialUARTWrite',          'serial/UART_Write_en');
@@ -71,50 +110,16 @@ function createStruct()
   addStruct('SerialCANConfig',          'serial/CAN_Config_en');
   addStruct('SerialCANReceive',         'serial/CAN_Receive_en');
   addStruct('SerialCANSend',            'serial/CAN_Send_en');
-  addStruct('Max11300_Config',          'converter_extender/Max11300_Config_en');
-  addStruct('Max11300_digitalInput',    'converter_extender/Max11300_Digital_Input_en');
-  addStruct('Max11300_digitalOutput',   'converter_extender/Max11300_Digital_Output_en');
-  addStruct('Max11300_analogInput',     'converter_extender/Max11300_Analog_Input_en');
-  addStruct('Max11300_analogOutput',    'converter_extender/Max11300_Analog_Output_en');
-  addStruct('Max11300_temperature',     'converter_extender/Max11300_Temperature_en');
-  addStruct('tmp102',                   'sensor/TMP102_en');
-  addStruct('lm75b',                    'sensor/LM75B_en');
-  addStruct('bmp180',                   'sensor/BMP180_en');
-  addStruct('vl6180x',                  'sensor/VL6180X_en');
-  addStruct('lsm303dlhc',               'sensor/LSM303DLHC_en');
-  addStruct('mpu9250',                  'sensor/MPU9250_en');
-  addStruct('mpu9250_ak8963',           'sensor/MPU9250_AK8963_en');
-  addStruct('mcp3428',                  'converter_extender/Mcp3428_Analog_Input_en');
-  addStruct('mcp4728',                  'converter_extender/Mcp4728_Analog_Output_en');
-  addStruct('PCF8574DigitalOutput',     'converter_extender/Pcf8574_Digital_Output_en');
-  addStruct('PCF8574DigitalInput',      'converter_extender/Pcf8574_Digital_Input_en');
-  addStruct('fm24cl16bRead',            'memory/Fm24cl16b_Fram_Read_en');
-  addStruct('fm24cl16bWrite',           'memory/Fm24cl16b_Fram_Write_en');
-  addStruct('mcp4822',                  'converter_extender/Mcp4822_Analog_Output_en');
-  addStruct('mcp3204',                  'converter_extender/Mcp3204_Analog_Input_en');
-  addStruct('tmp123',                   'sensor/TMP123_en');
-  addStruct('lis302dl',                 'sensor/LIS302DL_en');
-  addStruct('l3gd20',                   'sensor/L3GD20_en');
-  addStruct('max6675',                  'sensor/MAX6675_en');
-  addStruct('OneWireConfig',            'serial/one_wire_config_en');
-  addStruct('OneWireReadROM',           'serial/one_wire_search_rom_en');
-  addStruct('ds1820',                   'sensor/DS1820_en');
-  addStruct('DisplayCR12832',           'several/DisplayCR12832_en');
-  addStruct('DS3231',                   'several/DS3231_en');
-  addStruct('fm25l16bRead',             'memory/Fm25l16b_Fram_Read_en');
-  addStruct('fm25l16bWrite',            'memory/Fm25l16b_Fram_Write_en');
+
   addStruct('CSVWriter',                'several/CsvWriter_en');
   addStruct('CSVReader',                'several/CsvReader_en');
+
   addStruct('randomNumber',             'targets/target_specific/Random_Number_Generator_en');
   addStruct('timerEncoderInput',        'targets/target_specific/Encoder_Input_en');
   addStruct('timerInputCapture',        'targets/target_specific/InputCapture_en');
   addStruct('timerCounter',             'targets/target_specific/Counter_en');
-  addStruct('TLC5952Config',            'serial/TLC5952Config_en');
-  addStruct('TLC5962Ventil',            'serial/TLC5962Ventil_en');
-  addStruct('TLC5952Balken',            'serial/TLC5952Balken_en');
 
   addStruct('EthernetIPConfigStack',    'ethernet/EthernetIPConfigStack_en');
-
   addStruct('UDPConfigSocket',          'ethernet/UDPConfigSocket_en');
   %addStruct('UDPServerWrite',           'ethernet/UDPServerWrite_en');
   %addStruct('UDPServerWriteCSV',        'ethernet/UDPServerWriteCSV_en');
--- targets/libraries/LM75B/LM75B.cpp
+++ targets/libraries/LM75B/LM75B.cpp
@@ -2,7 +2,7 @@
 
 LM75B::LM75B(I2C *i2c) {
    char cmd[2];
-	 _i2c = i2c;
+  _i2c = i2c;
    cmd[0]    = LM75B_Conf;
    cmd[1]    = 0x0;   
    _i2c->write( LM75B_ADDR, cmd, 2);
@@ -10,15 +10,15 @@ LM75B::LM75B(I2C *i2c) {
 
 LM75B::~LM75B()
 {
- 
+
 }
- 
+
 float LM75B::read()
 {
     char cmd[2];
     cmd[0] = LM75B_Temp;
-   
+
     _i2c->write( LM75B_ADDR, cmd, 1); // Send command string
-    _i2c->read( LM75B_ADDR, cmd, 2); // Send command string    
+    _i2c->read( LM75B_ADDR, cmd, 2); // Reads 2 bytes
     return (   float((cmd[0]<<8)|cmd[1]) / 256.0   );
 }
