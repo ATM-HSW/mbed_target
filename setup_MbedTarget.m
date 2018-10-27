5adada1495de98bc2fcfcc87363f61aa01dd2883
ohagendorf <olaf.hagendorf@hs-wismar.de>
2018-10-27 21:03:57 +0200

    reorganizing setup m file

--- setup_MbedTarget.m
+++ setup_MbedTarget.m
@@ -1,5 +1,5 @@
 %  MbedTarget Simulink target
-%  Copyright (c) 2014-2017 Dr.O.Hagendorf , HS Wismar
+%  Copyright (c) 2014-2018 Dr.O.Hagendorf , HS Wismar
 %
 %  Licensed under the Apache License, Version 2.0 (the "License");
 %  you may not use this file except in compliance with the License.
@@ -60,30 +60,6 @@ if(size(ret,1)<=4 && size(ret1,1)>2)
 end
 
 
-% [ok, where, version] = getGCC();
-% if ok
-%     disp(' ');
-%     disp('found gcc version:');
-%     disp(version);
-%     disp('in folder:');
-%     disp(where);
-%     disp('All versions newer than 4.8 are OK for mbed_target');
-% else
-%     error('Can not find arm-none-eabi-gcc.exe. Please install GNU ARM Embedded Toolchain: gcc version 6.3.1 20170215 (release) [ARM/embedded-6-branch revision 245512] (GNU Tools for ARM Embedded Processors 6-2017-q1-update) and add the bin folder to System or User Path');
-% end
-
-% [ok, where, version] = getMake();
-% if ok
-%     disp(' ');
-%     disp('found make version:');
-%     disp(version);
-%     disp('in folder:');
-%     disp(where);
-%     disp('All versions newer than 3.81 are OK for mbed_target');
-% else
-%     error('Can not find make.exe. Please install GNU make 3.81vfor Windows or better and add the bin folder to System or User Path');
-% end
-
 [okPython, where, version] = getPython();
 if okPython
     disp(' ');
@@ -116,62 +92,6 @@ if okPython
         disp(['found ' int2str(c) ' boards with "project.py -S targets"']);
     end
 end
-% [ok, where, version] = getMbed();
-% if ok
-%     disp(' ');
-%     disp('found mbed-cli version');
-%     disp(version);
-%     disp('in folder:');
-%     disp(where);
-%     disp('All versions newer than 0.9.10 are OK for mbed_target and mbed5 compatibility');
-% else
-%     disp(' ');
-%     disp('Can not find mbed-cli.');
-%     if okPython
-%         choice = txtmenu('Do you want to install mbed-cli?','yes','no');
-%         if choice==0
-%             disp('Please check the output of the installation. Or rerun this setup.');
-%             system('pip install mbed-cli');
-%         else
-%             warning('When you want to use mbed5 targets, please install mbed with "pip install mbed-cli"');
-%         end
-%     else
-%         warning('When you want to use mbed5 targets, please install mbed with "pip install mbed-cli"');
-%     end
-% end
-
-[okMbedls, where, version] = getMbedls();
-if okMbedls
-    disp(' ');
-    disp('found mbed-ls version');
-    disp(version);
-    disp('in folder:');
-    disp(where);
-    disp('All versions newer than 1.2.13 are OK for mbed_target and mbed5 compatibility');
-    disp('When it is to old please call: "pip install mbed-ls --upgrade" at a Windows commandline')
-else
-    disp(' ');
-    disp('Can not find mbed-ls.');
-    if okPython
-        choice = txtmenu('Do you want to install mbed-ls?','yes','no');
-        if choice==0
-            disp('Please check the output of the installation. Or rerun this setup.');
-            system('pip install mbed-ls');
-        else
-            warning('When you want to use mbed5 targets, please install mbed with "pip install mbed-ls"');
-        end
-    else
-        warning('When you want to use mbed5 targets, please install mbedls with "pip install mbed-ls"');
-    end
-end
-if okMbedls
-    [status,out]=system('mbedls');
-    if(size(out,2)>1)
-        disp(' ');
-        disp('found a mbed board:')
-        disp(out);
-    end
-end
 
 try
     mbed_getTargetRootPath();
@@ -200,89 +120,5 @@ else
 end
 if ok
     version = out(strfind(out,'2.7'):end-1);
-end
-end
-
-% function [ok, where, version] = getGCC
-% [status,out]=system('where arm-none-eabi-gcc');
-% ok = false;
-% where = '';
-% version = '';
-% if status ~= 0
-%     return;
-% end
-% lines=strfind(out,10);
-% where = out(1:lines(1)-1);
-% [status,out]=system('arm-none-eabi-gcc --version');
-% if status ~= 0
-%     ok = false;
-% else
-%     ok = true;
-% end
-% if ok
-%     version = out(1:strfind(out,10)-1);
-% end
-% end
-
-% function [ok, where, version] = getMake
-% [status,out]=system('where make');
-% ok = false;
-% where = '';
-% version = '';
-% if status ~= 0
-%     return;
-% end
-% lines=strfind(out,10);
-% where = out(1:lines(1)-1);
-% [status,out]=system('make -v');
-% if status ~= 0
-%     ok = false;
-% else
-%     ok = true;
-% end
-% if ok
-%     version = out(1:strfind(out,10)-1);
-% end
-% end
-
-% function [ok, where, version] = getMbed
-% [status,out]=system('where mbed');
-% ok = false;
-% where = '';
-% version = '';
-% if status ~= 0
-%     return;
-% end
-% lines=strfind(out,10);
-% where = out(1:lines(1)-1);
-% [status,out]=system('mbed --version');
-% if status ~= 0
-%     ok = false;
-% else
-%     ok = true;
-% end
-% if ok
-%     version = out(1:strfind(out,10)-1);
-% end
-% end
-
-function [ok, where, version] = getMbedls
-[status,out]=system('where mbedls');
-ok = false;
-where = '';
-version = '';
-if status ~= 0
-    return;
-end
-lines=strfind(out,10);
-where = out(1:lines(1)-1);
-[status,out]=system('mbedls --version');
-if status ~= 0
-    ok = false;
-else
-    ok = true;
-end
-if ok
-    version = out(1:strfind(out,10)-1);
 end
 end
